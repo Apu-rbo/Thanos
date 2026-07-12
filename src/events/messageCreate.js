e282e4727b5345236b810266a84867c532f11f68
Apu-rbo <116592897+Apu-rbo@users.noreply.github.com>
2026-07-12 19:58:53 +0600

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,36 +1,19 @@
-
-
-
-
-
 import { Events } from 'discord.js';
 import { logger } from '../utils/logger.js';
 import { checkRateLimit } from '../utils/rateLimiter.js';
+import { handleLeveling } from '../services/leveling.js';
 
 const MESSAGE_XP_RATE_LIMIT_ATTEMPTS = 12;
 const MESSAGE_XP_RATE_LIMIT_WINDOW_MS = 10000;
 
 export default {
-  name: Events.MessageCreate,
-  async execute(message, client) {
-    try {
-      
-      if (message.author.bot || !message.guild) return;
-
-      await handleLeveling(message, client);
-    } catch (error) {
-      logger.error('Error in messageCreate event:', error);
+    name: Events.MessageCreate,
+    async execute(message, client) {
+        try {
+            if (message.author.bot || !message.guild) return;
+            await handleLeveling(message, client);
+        } catch (error) {
+            logger.error('Error in messageCreate event:', error);
+        }
     }
-  }
 };
-
-
-
-
-
-
-
-
-
-
-
